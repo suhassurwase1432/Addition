597b04f990cf96ce3f8a0bd57e68ef0b3b24b399
Suhas Surwase <suhassurwase1432@gmail.com>
2022-03-15 10:04:46 +0530

    first commit from eclipse

---
+++ src/Sorting/addition.java
@@ -0,0 +1,16 @@
+package Sorting;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10;
+		int b=86;
+		
+		System.out.println(a+b);
+		
+		System.out.println("Never lose Hope");
+	}
+
+}
